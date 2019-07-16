049850d091926a2d05251e293cff22e71ffa92ad
Valerie Manuel <valerie.manuel11@gmail.com>
2019-07-16 11:40:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,14 +17,14 @@ WIN_COMBINATIONS =[
  
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    # puts "-----------"
+    puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    # puts "-----------"
+    puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
   def input_to_index(user_input)
-    user_input.to_i - 1
+    user_input.do_i - 1
   end
   
   
