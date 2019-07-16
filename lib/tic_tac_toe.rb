a1a5ec9c76c63d2445f3af21ab81f6ba184339d2
Valerie Manuel <valerie.manuel11@gmail.com>
2019-07-16 11:38:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,9 +13,9 @@ WIN_COMBINATIONS =[
     [0,4,8],
     [2,4,6]
   ]
- 
  end 
-    def display_board
+ 
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
